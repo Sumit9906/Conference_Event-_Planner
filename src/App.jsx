d0aaca3a6416e84a6c8a33fe8515f56185801be0
Sumit9906 <sumitku9906@gmail.com>
2024-07-21 13:12:32 +0530

    build for production

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             <h1 className="budget_heading">Conference Expense Planner</h1>
             <p className="budget_sentence"> Plan your next major event with us!</p>
             <div className="getstarted_btn">
-              <button onClick={() => handleGetStarted()} className="get-started-btn">
+              <button onClick={()=>handleGetStarted()} className="get-started-btn">
                 Get Started
               </button>
             </div>
